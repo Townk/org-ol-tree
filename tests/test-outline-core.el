04c350c149dd322ca401aba8e32ee84159ae3826
Thiago Alves <thiago@rapinialves.com>
2021-03-30 13:36:07 -0500

    Fix core tests comments to match the actual code

--- tests/test-outline-core.el
+++ tests/test-outline-core.el
@@ -6,13 +6,13 @@
 ;;
 ;;; Commentary:
 ;;
-;; This file contains all tests related to the outline elements (currently
+;; This file contains all tests related to the core objects (currently
 ;; 'sections' and 'headings').
 ;;
 ;; Since reading the test might be tedious, I'm reproducing here the transcript
 ;; of the behaviors being tested by this file:
 ;;
-;; - A section object
+;; - Section
 ;;   - should be a list of integers or a string of integers separated by a '.'
 ;;   - when converting between list and string
 ;;     - should convert a section stack into a string
@@ -23,7 +23,7 @@
 ;;     - should return the next section stack when creating a new section on a given level
 ;;     - should throw an exception when trying to get next section from a non-section object
 ;;
-;; - A heading structure
+;; - Heading
 ;;   - should allow me to create an object with no slot values
 ;;   - when creating it from a non org buffer
 ;;     - should throw a user error with a non-org buffer message
@@ -35,6 +35,31 @@
 ;;       - should return a sibling heading when passing previous heading on same level
 ;;       - should return a sub-heading when passing previous heading on lower level
 ;;       - should return a heading when passing previous heading on higher level
+;;   - when getting it from the outline buffer
+;;     - should retrieve the heading by getting the :heading property of the button
+;;
+;; - Document
+;;   - the object model (DOM)
+;;     - retrieving the DOM from the Outline buffer
+;;       - when retrieving an already created and clean DOM
+;;         - should return the DOM without re-building it
+;;       - when retrieving an already created DOM with the rebuild flag
+;;         - should rebuild the DOM and return it
+;;     - when retrieving the DOM for the first time with no rebuild flag
+;;       - should rebuild the DOM and return it
+;;     - when retrieving the DOM for the first time with the rebuild flag
+;;       - should rebuild the DOM and return it
+;;   - when creating from a non Org file
+;;     - should fail with an user error
+;;   - when creating from an Org file
+;;     - should be able to traverse the document and return a heading DOM
+;;   - when defining its name
+;;     - from a file with no title
+;;       - should use the file name in title-case as the node name
+;;     - from a file with a defined title
+;;       - should use the text defined on the '#+TITLE:' property as the node name
+;;     - from an org buffer with no file and no title
+;;       - should use the buffer name in title-case as the node name
 ;;
 ;;; Code:
 
@@ -46,7 +71,8 @@
 
 (defconst test-root-dir (expand-file-name "tests"))
 
-(describe "Section object"
+
+(describe "Section"
   (it "should be a list of integers or a string of integers separated by a '.'"
     (expect (org-ol-tree-core--section-p '(3 2 1)) :to-be-truthy)
     (expect (org-ol-tree-core--section-p nil) :not :to-be-truthy)
@@ -100,7 +126,8 @@
       (expect (org-ol-tree-core--next-section '(3 nil 1) 1) :to-throw))))
 
 
-(describe "Heading structure"
+
+(describe "Heading"
   :var (empty-heading)
 
   (it "should allow me to create an object with no slot values"
@@ -179,100 +206,58 @@
         (setq new-heading (org-ol-tree-core--heading-create current-heading))
         (expect (org-ol-tree-core--heading-name new-heading) :to-equal "Heading 2")
         (expect (org-ol-tree-core--heading-id new-heading) :to-equal "2")
-        (expect (org-ol-tree-core--heading-level new-heading) :to-equal 1)))))
+        (expect (org-ol-tree-core--heading-level new-heading) :to-equal 1))))
 
 
-(describe "Document traversal"
-  (describe "When requested to traverse a non Org file"
-    (it "should fail with an user error"
-      (with-temp-buffer
-        (expect (org-ol-tree-core--doc-create (current-buffer))
-                :to-throw 'user-error '("Can’t traverse an Org document on a NON-Org buffer")))))
+  (describe "when getting it from the outline buffer"
+    (it "should retrieve the heading by getting the :heading property of the button"
+      (spy-on 'get-text-property :and-return-value nil)
 
-  (describe "When requested to traverse an Org file"
-    :var (doc-dom)
+      (org-ol-tree-core--heading-current)
 
-    (it "should be able to traverse the document and return a heading DOM"
-      (find-file (expand-file-name "data/doc-happy-path.org" test-root-dir))
-      (setq doc-dom (org-ol-tree-core--doc-create (current-buffer)))
-
-      (expect doc-dom :not :to-be nil)
-      (expect (length (org-ol-tree-core--heading-subheadings doc-dom)) :to-equal 3)
-
-      (kill-buffer (current-buffer)))))
+      (expect 'get-text-property :to-have-been-called-with nil :heading))))
 
 
-(describe "Document name"
-  (describe "on a file with no title"
+
+(describe "Document"
+
+  (describe "the object model (DOM)"
 
     (before-each
-      (find-file (expand-file-name "data/doc-no-title.org" test-root-dir)))
-
-    (after-each
-      (kill-buffer (current-buffer)))
-
-    (it "should use the file name in title-case as the node name"
-      (expect (org-ol-tree-core--doc-name) :to-equal "Doc No Title")))
-
-  (describe "on a file with a defined title"
-
-    (before-each
-      (find-file (expand-file-name "data/doc-happy-path.org" test-root-dir)))
-
-    (after-each
-      (kill-buffer (current-buffer)))
-
-    (it "should use the text defined on the '#+TITLE:' property as the node name"
-      (expect (org-ol-tree-core--doc-name) :to-equal "Document Title")))
-
-  (describe "on an org buffer with no file and no title"
-
-    (it "should use the buffer name in title-case as the node name"
-
-      (expect (with-current-buffer (create-file-buffer "my-non-file-org-buffer")
-                (org-ol-tree-core--doc-name))
-              :to-equal
-              "My Non File Org Buffer")))
-  )
+      (spy-on 'org-ol-tree-core--doc-create :and-return-value 'MyGeneratedDOM))
 
 
-(describe "Document Object Model"
-
-  (before-each
-    (spy-on 'org-ol-tree-core--doc-create :and-return-value 'MyGeneratedDOM))
-
-
-  (describe "retrieving the DOM from the Outline buffer"
-    :var (org-ol-tree--buffer-p
-          org-ol-tree--org-buffer)
-
-    (before-each
-      (setq-local org-ol-tree--buffer-p t
-                  org-ol-tree--org-buffer 'OrgBuffer))
-
-
-    (describe "when retrieving an already created and clean DOM"
-      :var (org-ol-tree-core--DOM org-ol-tree-core--rebuild-DOM-p)
+    (describe "retrieving the DOM from the Outline buffer"
+      :var (org-ol-tree--buffer-p
+            org-ol-tree--org-buffer)
 
       (before-each
-        (setq-local org-ol-tree-core--DOM 'MyDOM
-                    org-ol-tree-core--rebuild-DOM-p nil))
-
-      (it "should return the DOM without re-building it"
-        (expect (org-ol-tree-core--doc) :to-be 'MyDOM)
-        (expect 'org-ol-tree-core--doc-create :not :to-have-been-called)))
+        (setq-local org-ol-tree--buffer-p t
+                    org-ol-tree--org-buffer 'OrgBuffer))
 
 
-    (describe "when retrieving an already created DOM with the rebuild flag"
-      :var (org-ol-tree-core--DOM org-ol-tree-core--rebuild-DOM-p)
+      (describe "when retrieving an already created and clean DOM"
+        :var (org-ol-tree-core--DOM org-ol-tree-core--rebuild-DOM-p)
 
-      (before-each
-        (setq-local org-ol-tree-core--DOM 'MyDOM
-                    org-ol-tree-core--rebuild-DOM-p t))
+        (before-each
+          (setq-local org-ol-tree-core--DOM 'MyDOM
+                      org-ol-tree-core--rebuild-DOM-p nil))
 
-      (it "should rebuild the DOM and return it"
-        (expect (org-ol-tree-core--doc) :to-be 'MyGeneratedDOM)
-        (expect 'org-ol-tree-core--doc-create :to-have-been-called-with 'OrgBuffer))))
+        (it "should return the DOM without re-building it"
+          (expect (org-ol-tree-core--doc) :to-be 'MyDOM)
+          (expect 'org-ol-tree-core--doc-create :not :to-have-been-called)))
+
+
+      (describe "when retrieving an already created DOM with the rebuild flag"
+        :var (org-ol-tree-core--DOM org-ol-tree-core--rebuild-DOM-p)
+
+        (before-each
+          (setq-local org-ol-tree-core--DOM 'MyDOM
+                      org-ol-tree-core--rebuild-DOM-p t))
+
+        (it "should rebuild the DOM and return it"
+          (expect (org-ol-tree-core--doc) :to-be 'MyGeneratedDOM)
+          (expect 'org-ol-tree-core--doc-create :to-have-been-called-with 'OrgBuffer))))
 
 
     (describe "when retrieving the DOM for the first time with no rebuild flag"
@@ -296,19 +281,65 @@
 
       (it "should rebuild the DOM and return it"
         (expect (org-ol-tree-core--doc) :to-be 'MyGeneratedDOM)
-        (expect 'org-ol-tree-core--doc-create :to-have-been-called-with 'OrgBuffer)))
-  )
+        (expect 'org-ol-tree-core--doc-create :to-have-been-called-with 'OrgBuffer))))
 
 
-(describe "Outline tree view"
-  (it "should retrieve the heading by getting the :heading property of the button"
-    (spy-on 'get-text-property :and-return-value nil)
-
-    (org-ol-tree-core--heading-current)
-
-    (expect 'get-text-property :to-have-been-called-with nil :heading)
-    ))
+  (describe "when creating from a non Org file"
+    (it "should fail with an user error"
+      (with-temp-buffer
+        (expect (org-ol-tree-core--doc-create (current-buffer))
+                :to-throw 'user-error '("Can’t traverse an Org document on a NON-Org buffer")))))
 
 
+  (describe "when creating from an Org file"
+    :var (doc-dom)
+
+    (it "should be able to traverse the document and return a heading DOM"
+      (find-file (expand-file-name "data/doc-happy-path.org" test-root-dir))
+      (setq doc-dom (org-ol-tree-core--doc-create (current-buffer)))
+
+      (expect doc-dom :not :to-be nil)
+      (expect (length (org-ol-tree-core--heading-subheadings doc-dom)) :to-equal 3)
+
+      (kill-buffer (current-buffer))))
+
+
+  (describe "when defining its name"
+
+    (describe "from a file with no title"
+
+      (before-each
+        (find-file (expand-file-name "data/doc-no-title.org" test-root-dir)))
+
+      (after-each
+        (kill-buffer (current-buffer)))
+
+      (it "should use the file name in title-case as the node name"
+        (expect (org-ol-tree-core--doc-name) :to-equal "Doc No Title")))
+
+
+    (describe "from a file with a defined title"
+
+      (before-each
+        (find-file (expand-file-name "data/doc-happy-path.org" test-root-dir)))
+
+      (after-each
+        (kill-buffer (current-buffer)))
+
+      (it "should use the text defined on the '#+TITLE:' property as the node name"
+        (expect (org-ol-tree-core--doc-name) :to-equal "Document Title")))
+
+
+    (describe "from an org buffer with no file and no title"
+
+      (it "should use the buffer name in title-case as the node name"
+
+        (expect (with-current-buffer (create-file-buffer "my-non-file-org-buffer")
+                  (org-ol-tree-core--doc-name))
+                :to-equal
+                "My Non File Org Buffer")))))
+
+
+
 (provide 'test-outline-core)
 ;;; test-outline-core.el ends here
